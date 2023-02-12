f9c7adef20e4284d8bc4c7ef52ed855fbbdefb97
LandonBuell <42503615+landonbuell@users.noreply.github.com>
2023-02-12 16:40:51 -0500

    Added new TreasureHunterCore project

---
+++ TreasureHunter/TreasureHunterCore/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Repository:      TreasureHunter
+ * Solution:        TreatureHunter
+ * Project:         TreasureHunterCore
+ * Namespace:       Core
+ * 
+ * File:            Program.cs
+ * Author:          Landon Buell
+ * Date:            Feb 2023 
+ */
+
+namespace TreasureHunterCore
+{
+    class TreaureHunterCoreMain
+    {
+        static void Main(string[] args)
+        {
+            // Main Executable
+
+        }
+    }
+}
