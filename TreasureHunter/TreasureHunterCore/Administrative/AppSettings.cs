4511957168eb18448e3fe8ea5e24e968491adb7c
LandonBuell <42503615+landonbuell@users.noreply.github.com>
2023-02-12 23:17:52 -0500

    Added TextLogger, updates AppSettings

--- TreasureHunter/TreasureHunterCore/Administrative/AppSettings.cs
+++ TreasureHunter/TreasureHunterCore/Administrative/AppSettings.cs
@@ -9,11 +9,76 @@
  * Date:            Feb 2023 
  */
 
+using System.IO;
+
 namespace TreasureHunterCore.Administrative
 {
     internal class AppSettings
     {
+        // Store all Runtime Settings
 
+        private string _pathStartup;
+        private string _pathOutput;
+
+        private TextLogger.LogLevel _logLevel;
+        private bool _logToFile;
+        private bool _logToConsole;
+
+        private bool _isDebugMode;
+
+        public AppSettings()
+        {
+            // Constructor
+            _pathStartup = Directory.GetCurrentDirectory();
+            _pathOutput = Path.Combine(_pathStartup, "..", "..", "outputLog");
+
+            _logLevel = TextLogger.LogLevel.MESSAGE;
+            _logToFile = true;
+            _logToConsole = false;
+
+            _isDebugMode = true;
+        }
+
+        #region Getters and Setters
+
+        public string StartupPath
+        {
+            // Get the App's Startup Path
+            get { return _pathStartup; }
+        }
+
+        public string OutputPath
+        {
+            // Get the App's Output Logging Path
+            get { return _pathOutput; }
+        }
+
+        public TextLogger.LogLevel MinimumLogLevel
+        {
+            // Get the Logger's Minimum Required Level
+            get { return _logLevel; }
+        }
+
+        public bool LogToFile
+        {
+            // Get if messages should be logged to specified file
+            get { return _logToFile; }
+        }
+
+        public bool LogToConsole
+        {
+            // Get if messages should be logged to specified file
+            get { return _logToConsole; }
+        }
+
+        public bool IsDebugMode
+        {
+            // Get T/F If app is in Debug Mode
+            get { return IsDebugMode; } 
+        }
+
+
+        #endregion
 
         #region Public Interface
 
---
+++ TreasureHunter/TreasureHunterCore/Administrative/TextLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreasureHunterCore.Administrative
+{
+    internal class TextLogger
+    {
+        // Log Text to Console or To File
+        private string _outputFile;
+        private LogLevel _logLevel;
+
+        private bool _logToConsole;
+        private bool _logToFile;
+
+        internal enum LogLevel
+        {
+            // Enumeration for Log Level Severity
+            TRACE = 0,
+            INFO = 1,
+            MESSAGE = 2,
+            WARNING = 3,
+            ERROR = 4,
+            FATAL = 5
+        }
+
+
+
+
+    }
+}
